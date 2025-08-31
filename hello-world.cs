27c80ac95e14e5c2b8c7d90ca9a5f2b8b844df3f
Wondrfish <166275616+Wondrfish@users.noreply.github.com>
2025-08-30 20:46:12 -0400

    Create hello-world.cs

---
+++ hello-world.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Myfirsstprogram
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I like pizza");
+           // makes a beep (lol) Console.Beep();
+         }
+     }
+ 
+}
