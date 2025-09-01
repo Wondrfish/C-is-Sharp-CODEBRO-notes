5f7687b56017a6b289e329b1661de1bdb4eec97b
Wondrfish <166275616+Wondrfish@users.noreply.github.com>
2025-09-01 15:40:09 -0400

    Create Const.cs

---
+++ Const.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Myfirsstprogram
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            /* constants = immutable values which are known at compilte time and do mot change for the liffe off the program
+             */
+            const double pi = 3.14159;
+
+            Console.WriteLine(pi); 
+
+
+        }
+     }
+ 
+}
